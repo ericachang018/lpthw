d55a304ea84a77262daf259573feabb2ad0d0555
echang <echang@kixeye.com>
2013-10-21 10:10:24 -0700

    Exercise 3 - played around with floating point numbers

--- ex3.py
+++ ex3.py
@@ -2,8 +2,8 @@
 # yey I can do a whole bunch of things with math! 
 
 print "I will now count my chinkens:"
-print "Hens", 25+30 /6 
-print "Roosters", 100 - 25*3%4 
+print "Hens", 25.00+30.00 /6.00 
+print "Roosters", 100.00 - 25.00*3.00%4.00 
 print "Now I will count the eggs:"
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 print "Is it true that 3 + 2 < 5 - 7?"
@@ -15,4 +15,11 @@ print "How about some more."
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
- 
+
+#Playing around with floating point numbers
+
+print "3/5=",3/5
+print "3.0/5.0000=",3.0/5.0000
+print "3/5.0=",3/5.0
+
+
