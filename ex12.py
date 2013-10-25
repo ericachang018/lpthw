7791e806094d524f7ebee98435f9114238cf05cf
echang <echang@kixeye.com>
2013-10-25 15:28:50 -0700

    Exercise 12

---
+++ ex12.py
@@ -0,0 +1,12 @@
+# Exercise 12 : Prompting People 
+
+# In the terminal you can get some pydocs directly by typing 
+# >> pydoc raw_input (or anything else you'd like to read up on) 
+
+age = raw_input ("How old are you?")
+height = raw_input ("how tall are you?")
+weight = raw_input("How much do you weight?")
+
+print "so you are %r old, %r tall and %r heavy?" % (age, height, weight)
+
+
