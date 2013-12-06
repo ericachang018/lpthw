8588f8790000f838208281c45e7d32570b1a839f
Erica Chang <echang@kixeye.com>
2013-12-06 10:00:44 -0800

    Exercise 20 Functions and files

--- ex20.py
+++ ex20.py
@@ -1,3 +1,5 @@
+# Exercise 20 functions and files 
+
 from sys import argv
 script, input_file = argv
 
@@ -27,6 +29,8 @@ print_a_line(current_line, current_file)
 
 current_line = current_line + 1
 print_a_line(current_line, current_file)
+
+
 
 
 
