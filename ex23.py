5f8df01368f6387790a29b7779088b2fe778f838
Erica Chang <echang@kixeye.com>
2013-12-15 22:37:38 -0800

    Another study drill exercise on reading code

---
+++ ex23.py
@@ -0,0 +1,11 @@
+# Exercise 23 - Reading code on the internet! 
+#
+
+# Here's what you do:
+
+# Go to bitbucket.org, github.com, or gitorious.org with your favorite web browser and search for "python."
+# Avoid any project that mentions "Python 3". That'll only confuse you.
+# Pick a random project and click on it.
+# Click on the Source tab and browse through the list of files and directories until you find a .py file (but not setup.py, that's useless).
+# Start at the top and read through it, to take notes on what you think it does.
+# If any symbols or strange words seem to interest you, write them down to research later.
