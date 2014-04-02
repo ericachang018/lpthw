6d16852f3deb2efbf034c3acc553c7b75ab9ad43
Erica Chang <ericachang018@gmail.com>
2014-04-02 02:48:55 -0700

    accessing items from a list

--- ex34.py
+++ ex34.py
@@ -1,2 +1,13 @@
 # exercise 33 accessing elements in a list 
 
+animals = ['bear', 'python','peacock', 'kangaroo','whale']
+print animals
+
+animal = animals[1]
+print "accessing the animal at 1 %s" % animal
+
+animal = animals[2]
+print "accessing hte animal at third %s" % animal
+
+# and so on and so forth I'm a little lazy so i'm not going to do the whole exercise
+
