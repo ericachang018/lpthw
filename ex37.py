d5743b15ab184c203c34b572cf6a642fb03a1def
Erica Chang <ericachang018@gmail.com>
2014-04-02 03:21:39 -0700

    started this exercise will need to finish it later

---
+++ ex37.py
@@ -0,0 +1,107 @@
+Keywords
+and - this and this 
+del - delete 
+from - kind of like a range 
+not - not including this 
+while - while something is true do a loop:
+as 
+elif - else if 
+global - global variables are not inside any scope
+or - this or this 
+with 
+assert - validate that this is true or false or whatever
+else - else to the if else statment 
+if - if to the if else statement 
+pass - skip
+yield 
+break - break out of the function move on to the next thing
+except - 
+import - use a library or something inside a library 
+print - will print to the terminal 
+class - is an object Like a function but this creates an object
+exec 
+in
+raise
+continue
+finally
+is
+return
+def
+for
+lambda
+try
+Data Types
+For data types, write out what makes up each one. For example, with strings write out how you create a string. For numbers write out a few numbers.
+
+True = make soemthing true 
+False = make someting false
+None = There is nothing there not even a 0 
+strings  = "strings are in quotes"
+numbers = numbers are whole numbers
+floats = flots are numbers with decimals 
+lists = list[1,2,3,4]
+String Escape Sequences
+For string escape sequences, use them in strings to make sure they do what you think they do.
+
+\\
+\'
+\"
+\a
+\b
+\f
+\n
+\r
+\t
+\v
+String Formats
+Same thing for string formats: use them in some strings to know what they do.
+
+%d - replace with diget 
+%i - replace with interger
+%o
+%u
+%x
+%X
+%e
+%E
+%f
+%F
+%g
+%G
+%c
+%r - reper if its a string diget or interger put it is as it is in the list
+%s - string
+%%
+Operators
+Some of these may be unfamiliar to you, but look them up anyway. Find out what they do, and if you still can't figure it out, save it for later.
+
++ - Adds two intergers
+- - Subtracts 2 ingergers
+* - multiplies 2 intergers
+** - ex 2 ** 3 will result in 2 * 2 * 2  = 8
+/ - will divide 2 intergers 
+// 
+% - will give you the remainder of 2 intergers
+< - less than
+> - greater than 
+<= - less than or equal too 
+>= - greater than or equal too
+== - it is equal its this is exactally this on the other side of the operand not setting a variable 
+!= - not equal 
+<> 
+( ) - at the end of a function
+[ ] - makes a list
+{ } - makes a dictionary
+@
+,
+: - at the end of a clause 
+.
+= - setting variables 
+;
++= - will add one more ect 
+-= - will minus one more ie 1 -= will subtract 1 each time 
+*=
+/=
+//=
+%=
+**=
